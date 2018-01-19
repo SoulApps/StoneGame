c0f61692f6094e06c12ff6f81fac60d90a37ac8a
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-19 09:13:38 +0100

    v 1.0 game finished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager  {
+    public static int stonesThrown = 0;
+    public static int stonesDestroyed = 0;
+	
+}
--- Assets/Scripts/MainLoop.cs
+++ Assets/Scripts/MainLoop.cs
@@ -1,34 +1,73 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainLoop : MonoBehaviour {
-    // Array de GameObject donde sus componentes cero, uno y dos
-    // van a ser los tres prefab Stone que hemos creado
+public class MainLoop : MonoBehaviour
+{
+
     public GameObject[] stones = new GameObject[3];
-    // Fuerza de torsión que vamos a aplicar a cada uno de los ejes para que
-    // la piedra tenga rotación inicial al crearse
+    // Fuerza de torsion que vamos a aplicar en cada eje
     public float torque = 5.0f;
-    // Valores que vamos a tener ccomo fuerza cuando lancemos las piedras, 
-    // que será un valor aleatorio entre 20 y 40
-    public float minAntiGravity = 5.0f, maxAntiGravity = 40.0f;
-    // Fuerza lateral con las que lanzarán las piedras, en rango entre 5 y 15
-    public float minLateralForce = 15.0f, maxLateralForce = 15.0f;
-    // Tiempo que vamos a tener que esperar entre el lanzamiento de una piedra
-    // y la siguiente
+    // Valores de fuerza que van a tener las piedras cuando las lancemos, y sera un valor aleatorio entre 20 y 40
+    public float minAntiGravity = 20.0f, maxAntiGravity = 40.0f;
+    // Valores de fuerza lateral cuando las lancemos 
+    public float minLateralForce = -15.0f, maxLateralForce = 15.0f;
+    // Tiempo que vamos a tener que esperar entre el lanzamiento de una piedra y la siguiente
     public float minTimeBetweenStones = 1f, maxTimeBetweenStones = 3f;
     // Posición X del lanzamiento de la piedra
     public float minX = -30.0f, maxX = 30.0f;
     // Posición Z del lanzamiento de la piedra
     public float minZ = -5.0f, maxZ = 5.0f;
+    //Permitir que se lancen piedras
+    private bool enableStones = true;
+    private Rigidbody rigibody;
 
-    // changes
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int amountStones = 20;
+
+
+    void Start()
+    {
+        StartCoroutine(ThrowStones());
+    }
+
+    private IEnumerator ThrowStones()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        while (enableStones)
+        {
+            GameObject stone = Instantiate(stones[Random.Range(0, stones.Length)]);
+            stone.transform.position = new Vector3(Random.Range(minX, maxX), -30.0f, Random.Range(minZ, maxZ));
+            stone.transform.rotation = Random.rotation;
+            rigibody = stone.GetComponent<Rigidbody>();
+            rigibody.AddTorque(Vector3.up * torque, ForceMode.Impulse);
+            rigibody.AddTorque(Vector3.right * torque, ForceMode.Impulse);
+            rigibody.AddTorque(Vector3.forward * torque, ForceMode.Impulse);
+
+            rigibody.AddForce(Vector3.up * Random.Range(minAntiGravity, maxAntiGravity), ForceMode.Impulse);
+            rigibody.AddForce(Vector3.right * Random.Range(minLateralForce, maxLateralForce), ForceMode.Impulse);
+
+            GameManager.stonesThrown++;
+
+            if(GameManager.stonesThrown == amountStones)
+            {
+                enableStones = false;
+                yield return new WaitForSeconds(6.0f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(minTimeBetweenStones, maxTimeBetweenStones));
+            }
+        }
+        SceneManager.LoadScene("Final");
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
